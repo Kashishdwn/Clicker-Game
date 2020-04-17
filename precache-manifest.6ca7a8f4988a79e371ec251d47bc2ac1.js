02f051c8836e93a065fd83bd32c1908a49f456c5
Kashishdwn <kashishdwn@gmail.com>
2020-04-18 01:36:03 +0530

    Updates

---
+++ precache-manifest.6ca7a8f4988a79e371ec251d47bc2ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ac46673d7fd2e95e06d76469afa5b3",
+    "url": "/Clicker-Game/index.html"
+  },
+  {
+    "revision": "ee4adcc20b41d93997fc",
+    "url": "/Clicker-Game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c5b33a135b4389654e77",
+    "url": "/Clicker-Game/static/js/2.0530ef78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicker-Game/static/js/2.0530ef78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4adcc20b41d93997fc",
+    "url": "/Clicker-Game/static/js/main.b9fa015f.chunk.js"
+  },
+  {
+    "revision": "20015c571752aa081ca9",
+    "url": "/Clicker-Game/static/js/runtime-main.36b81bae.js"
+  }
+]);
